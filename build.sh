f646d7b5edcd0245115ba03445478cdbe978b147
Ian Blenke <ian@blenke.com>
2017-10-20 20:28:10 -0400

    Try tagging docker images appropriately

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ docker build \
   --build-arg ELASTICSEARCH_TARBALL_SHA1=$ES_SHA1 \
   --build-arg XPACK_VERSION=$XP_VERSION \
   --build-arg XPACK_TARBALL_SHA1=$XP_SHA1 \
-  -t ${TRAVIS_REPO_SLUG}:v$ES_VERSION \
+  -t ${TRAVIS_REPO_SLUG}:latest \
   .
--- push.sh
+++ push.sh
@@ -2,10 +2,10 @@
 . .env
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push ${TRAVIS_REPO_SLUG}:latest
   set -x
-  docker tag ${TRAVIS_REPO_SLUG}:latest ${TRAVIS_REPO_SLUG}:v$ES_VERSION
-  docker push ${TRAVIS_REPO_SLUG}:v$ES_VERSION
   docker tag ${TRAVIS_REPO_SLUG}:latest ${TRAVIS_REPO_SLUG}:${TRAVIS_BUILD_NUMBER} 
+  docker tag ${TRAVIS_REPO_SLUG}:latest ${TRAVIS_REPO_SLUG}:v$ES_VERSION
+  docker push ${TRAVIS_REPO_SLUG}:latest
   docker push ${TRAVIS_REPO_SLUG}:${TRAVIS_BUILD_NUMBER}
+  docker push ${TRAVIS_REPO_SLUG}:v$ES_VERSION
 fi
