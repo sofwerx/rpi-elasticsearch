7e6de635b5231ba0ee44860d5622a4adaf3c8869
Ian Blenke <ian@blenke.com>
2017-10-20 20:11:59 -0400

    Renamed forked repo from ianblenke to sofwerx, changed REPO mentions to TRAVIS_REPO_PLUG, added DEBIANFRONTEND=noninteractive to silence debconf build warnings

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # rpi-elasticsearch
 
-[![Build Status](https://travis-ci.org/ianblenke/rpi-elasticsearch)](https://travis-ci.org/ianblenke/rpi-elasticsearch)
+[![Build Status](https://travis-ci.org/sofwerx/rpi-elasticsearch)](https://travis-ci.org/sofwerx/rpi-elasticsearch)
 
-This is the Dockerfile behind `ianblenke/rpi-elasticsearch` on Docker Hub, setup to autobuild through travis.
+This is the Dockerfile behind `sofwerx/rpi-elasticsearch` on Docker Hub, setup to autobuild through travis.
--- push.sh
+++ push.sh
@@ -2,6 +2,10 @@
 . .env
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-  docker push ${REPO}:v$ES_VERSION
-  docker push ${REPO}:latest
+  docker push ${TRAVIS_REPO_SLUG}:latest
+  set -x
+  docker tag ${TRAVIS_REPO_SLUG}:latest ${TRAVIS_REPO_SLUG}:v$ES_VERSION
+  docker push ${TRAVIS_REPO_SLUG}:v$ES_VERSION
+  docker tag ${TRAVIS_REPO_SLUG}:latest ${TRAVIS_REPO_SLUG}:${TRAVIS_BUILD_NUMBER} 
+  docker push ${TRAVIS_REPO_SLUG}:${TRAVIS_BUILD_NUMBER}
 fi
