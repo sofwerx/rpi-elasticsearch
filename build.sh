a0414da031c2cde8d22ec76cd1573b1fad8fa9ef
Ian Blenke <ian@blenke.com>
2017-10-25 08:05:55 -0400

    Adding cache-from for docker image build

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ docker build \
   --build-arg ELASTICSEARCH_TARBALL_SHA1=$ES_SHA1 \
   --build-arg XPACK_VERSION=$XP_VERSION \
   --build-arg XPACK_TARBALL_SHA1=$XP_SHA1 \
+  --cache-from ${TRAVIS_REPO_SLUG}:latest \
   -t ${TRAVIS_REPO_SLUG}:latest \
   .
